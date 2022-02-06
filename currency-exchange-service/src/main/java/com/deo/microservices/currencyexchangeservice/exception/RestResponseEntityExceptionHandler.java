b28c9fac4c18f69517f5004851b572e80dee2ff5
s.lukianenko <lukyanenko88@gmail.com>
2022-02-06 14:05:59 +0200

    add base for exception handling

---
+++ currency-exchange-service/src/main/java/com/deo/microservices/currencyexchangeservice/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.deo.microservices.currencyexchangeservice.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Object> handleRuntime(RuntimeException ex, WebRequest request){
+        String message = ex.getMessage();
+
+        return handleExceptionInternal(ex,message,new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
