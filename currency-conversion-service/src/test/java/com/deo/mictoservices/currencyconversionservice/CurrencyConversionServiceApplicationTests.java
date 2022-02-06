61e7e6a9b38398b2aa7b9485ed706c71fbf6d959
s.lukianenko <lukyanenko88@gmail.com>
2022-02-06 18:34:35 +0200

    naming server added

---
+++ currency-conversion-service/src/test/java/com/deo/mictoservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deo.mictoservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
