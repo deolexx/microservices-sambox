fc108522004006f4278ceb2ae575d8860f853f88
s.lukianenko <lukyanenko88@gmail.com>
2022-02-19 17:35:05 +0200

    new conversion multiple service added

---
+++ conversion-multiple-service/src/test/java/com/deo/microservices/conversionmultipleservice/ConversionMultipleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deo.microservices.conversionmultipleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionMultipleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
