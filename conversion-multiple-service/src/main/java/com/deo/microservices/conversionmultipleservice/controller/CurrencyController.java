7d24e2d40d637795efdacfdeb52408bc180bf9bf
s.lukianenko <lukyanenko88@gmail.com>
2022-02-19 19:43:53 +0200

    CurrencyController added

---
+++ conversion-multiple-service/src/main/java/com/deo/microservices/conversionmultipleservice/controller/CurrencyController.java
@@ -0,0 +1,11 @@
+package com.deo.microservices.conversionmultipleservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyController {
+
+
+
+
+}
--- conversion-multiple-service/src/main/java/com/deo/microservices/conversionmultipleservice/model/Currency.java
+++ conversion-multiple-service/src/main/java/com/deo/microservices/conversionmultipleservice/model/Currency.java
@@ -1,9 +1,16 @@
 package com.deo.microservices.conversionmultipleservice.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.math.BigDecimal;
+
 @RedisHash("Currency")
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "conversionMultiple")
 public class Currency {
     private String id;
     private String code;
