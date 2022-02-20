7d0968e73e55c80a0cd4495b26ff382aa6f0f9d4
s.lukianenko <lukyanenko88@gmail.com>
2022-02-20 16:43:53 +0200

    added new service to work with https://freecurrencyapi.net/

---
+++ freecurrencyapi-facade/src/main/java/com/deo/microservices/freecurrencyapifacade/FreecurrencyapiFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.deo.microservices.freecurrencyapifacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreecurrencyapiFacadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreecurrencyapiFacadeApplication.class, args);
+    }
+
+}
