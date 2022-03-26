d218b31a8e3c9b5d2a1ff33bff1609b21b1c8b0a
s.lukianenko <lukyanenko88@gmail.com>
2022-03-26 14:50:43 +0200

    create pdf export service

---
+++ pdf-export-service/src/main/java/com/deo/microservices/pdfexportservice/PdfExportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deo.microservices.pdfexportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfExportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfExportServiceApplication.class, args);
+    }
+
+}
