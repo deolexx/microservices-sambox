158f0e44cffb1d1e82f2e79c83902bc930e93559
s.lukianenko <lukyanenko88@gmail.com>
2022-04-16 14:19:05 +0300

    split configs in gateway service+ security config configured and tested

--- api-gateway/src/main/java/com/deo/microservices/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/deo/microservices/apigateway/config/SecurityConfig.java
@@ -1,27 +1,25 @@
 package com.deo.microservices.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails sam = User.withDefaultPasswordEncoder()
-                .username("sam")
-                .password("1234")
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User
+                .withUsername("user")
+                .password(encoder.encode("password"))
                 .roles("USER")
                 .build();
-        UserDetails frodo = User.withDefaultPasswordEncoder()
-                .username("frodo")
-                .password("1111")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(sam, frodo);
+        return new MapReactiveUserDetailsService(user);
     }
+
 }
