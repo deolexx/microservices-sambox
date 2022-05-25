7b128752c34e4380e22ce5e07074b49ca669c322
s.lukianenko <lukyanenko88@gmail.com>
2022-05-25 09:25:09 +0300

    WS client service

---
+++ websocker-client-service/src/main/java/com/deo/microservices/websockerclientservice/WebsockerClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deo.microservices.websockerclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockerClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockerClientServiceApplication.class, args);
+	}
+
+}
